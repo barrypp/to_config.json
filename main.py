22bd40b3410857d65da28e806dbd8096e7437efc
Barrypp.zzx <4166765+barrypp@users.noreply.github.com>
2023-04-25 08:57:38 +0800

    new

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import base64
+import re
+import json
+
+import sys
+sys.path.append(".")
+import var
+
+lines = base64.b64decode(requests.get(var.url).text)
+j0 = json.loads(open("config.json").read())
+
+for line in lines.splitlines():
+    line = line.replace(b"vmess://", b"")
+    j = json.loads(base64.b64decode(line+b"==="))
+    j0[var.j1][0][var.j2][var.j3][0]['address'] = j['add']
+    j0[var.j1][0][var.j2][var.j3][0]['port'] = j['port']
+    j0[var.j1][0][var.j2][var.j3][0]['users'][0]['id'] = j['id']
+    name = re.compile('s[0-9]+').search(j['add']).group(0)
+    open(f"config_{name}.json", "w").write(json.dumps(j0, indent=4))
